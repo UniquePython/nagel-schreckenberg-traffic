3253b5b0ba2095915d0cb81071aca478a3e73d9a
Ananyo Bhattacharya <ananyobhattacharya10@gmail.com>
2026-03-31 17:50:26 +0530

    implement function to calculate number of empty cells ahead

--- src/main.c
+++ src/main.c
@@ -1,7 +1,8 @@
 #include <raylib.h>
+#include <stddef.h>
 
 #define ROAD_LEN 300
-#define MAX_VELOCTY 5
+#define MAX_VELOCITY 5
 #define N_CARS 100
 #define DAWDLE_PROB 0.3f
 
@@ -12,6 +13,25 @@
 int g_curr_road[ROAD_LEN];
 int g_next_road[ROAD_LEN];
 
+// Returns number of empty cells ahead of position pos before hitting a car
+// Must wrap around (circular road)
+size_t gap_ahead(size_t pos)
+{
+    size_t nempty = 0;
+
+    for (size_t d = 1; d <= MAX_VELOCITY; d++)
+    {
+        size_t i = (pos + d) % ROAD_LEN;
+
+        if (g_curr_road[i] == -1)
+            nempty++;
+        else
+            break;
+    }
+
+    return nempty;
+}
+
 int main(void)
 {
     InitWindow(SCREEN_W, SCREEN_H, "Nagel-Schreckenberg Traffic");
@@ -20,7 +40,9 @@ int main(void)
     while (!WindowShouldClose())
     {
         BeginDrawing();
+
         ClearBackground(BLACK);
+
         EndDrawing();
     }
 
